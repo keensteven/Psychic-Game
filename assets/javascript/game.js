9dd023632949433f628f67e028087e972556af56
keensteven <keenstevie@gmail.com>
2019-05-02 18:17:40 -0400

    completed psychic homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    //  document.onkeyup = function(event) {
+
+        // Captures the key press, converts it to lowercase, and saves it to a variable.
+       var userGuess = event.key.toLowerCase();      var userGuess = prompt("How much do you rock?");
+
+        for (var i = 0; i < letters.length; i++) {
+            console.log("Correct guess is " + letters[i]);
+          }
+// if user guess = computer guess add 1 to wins
+             if userGuess =
+
+// if user guess !== computer guess add 1 to losses
+
+
